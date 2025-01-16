55191f9d0be367a53cf148e1cd9b7c21da55c3a5
Dileep <dileepdilee58@gmail.com>
2025-01-16 08:29:44 +0530

    basic project completed

--- backend.js
+++ backend.js
@@ -1,50 +1,24 @@
-
-
-
-// const http = require("http");
-// const fs = require("fs");
-
-
-
-// http.createServer((req, res) => {
-//     if (req.url === "/") {
-//         fs.readFile("/index.html", (err, data) => {
-//             if (err) {
-//                 res.writeHead(400, { "Content-Type": "text/plain" });
-//                 res.end("Home page loading failed");
-//             } else {
-//                 res.writeHead(200, { "Content-Type": "text/html" });
-//                 res.end(data);
-//             }
-//         });
-//     } else {
-//         res.writeHead(404, { "Content-Type": "text/plain" });
-//         res.end("Not Found");
-//     }
-   
-        
-    
-// }).listen(8500, () => {
-//     console.log("Server running at http://localhost:8500");
-// });
-
-
-
 const fs = require('fs');
 const path = require('path');
 const http = require('http');
 
-
 http.createServer((req, res) => {
     if (req.url === "/") {
-        // Correct path to index.html
-        // const filePath = path.join(__dirname, 'index.html');
-        // console.log(filePath)
-
-        fs.readFile("index2.html", (err, data) => {
+        // Correct path to cvhomepage.html
+        fs.readFile("cvhomepage.html", (err, data) => {
             if (err) {
                 res.writeHead(400, { "Content-Type": "text/plain" });
                 res.end("Home page loading failed");
+            } else {
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.end(data);
+            }
+        });
+    } else if (req.url==="/button-click") {
+        fs.readFile("index2.html", (err, data) => {
+            if (err) {
+                res.writeHead(400, { "Content-Type": "text/plain" });
+                res.end("Html page loading failed");
             } else {
                 res.writeHead(200, { "Content-Type": "text/html" });
                 res.end(data);
@@ -55,5 +29,5 @@ http.createServer((req, res) => {
         res.end("Not Found");
     }
 }).listen(8500, () => {
-    console.log(`Server running at http://localhost:8500`);
+    console.log("Server running at http://localhost:8500");
 });
